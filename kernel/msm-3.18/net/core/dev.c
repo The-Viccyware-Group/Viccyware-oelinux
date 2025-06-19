da15d2b28683214891c28803ff19a8b0c73df4c6
OpenEmbedded <oe.patch@oe>
2025-06-19 13:18:29 -0500

    everything except for camera works

--- kernel/msm-3.18/net/core/dev.c
+++ kernel/msm-3.18/net/core/dev.c
@@ -3448,8 +3448,13 @@ static int netif_rx_internal(struct sk_buff *skb)
 
 	trace_netif_rx(skb);
 #ifdef CONFIG_RPS
+<<<<<<< HEAD
 	//WARN_ONCE(skb_cloned(skb), "Cloned packet from dev %s\n",
 	//	  skb->dev->name);
+=======
+	WARN_ONCE(skb_cloned(skb), "Cloned packet from dev %s\n",
+		  skb->dev->name);
+>>>>>>> 255921fbb (everything except for camera works)
 
 	if (static_key_false(&rps_needed)) {
 		struct rps_dev_flow voidflow, *rflow = &voidflow;
