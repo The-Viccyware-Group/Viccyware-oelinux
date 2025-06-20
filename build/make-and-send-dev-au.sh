3b5b9c095dc3ad4daacb16e38a5712fb9421dba3
OpenEmbedded <oe.patch@oe>
2025-06-19 22:02:23 -0400

    what the fudge

--- build/make-and-send-dev-au.sh
+++ build/make-and-send-dev-au.sh
@@ -10,11 +10,18 @@ export SERVER_KEY_PATH="$path"
 
 I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt dev -v "$INCREMENT" -au
 I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt oskr -bp annul-burl-zq-flew-hack-owe-phil-triton-pk -v "$INCREMENT" -au
+
+scp -i "$SERVER_KEY_PATH" "_build/Viccyware-0.5.0.${INCREMENT}oskr.ota" root@froggitti.xyz:/home/cozmo/Desktop/all_servers/Viccyware-ota-server-dev/vicw-dev/otas/full/oskr/0.5.0."${INCREMENT}".ota
+
 I_AM_THE_CREATOR_AND_WANT_TO_MAKE_THE_BUILD_AUTO_UPDATE=1 ./build/build.sh -bt prod -s -op alike-essay-added-spumoni-lotion -bp require-radiolog-sprinkle-slither-genetics -v "$INCREMENT" -au
 
 scp -i "$SERVER_KEY_PATH" "_build/Viccyware-0.5.0.${INCREMENT}d.ota" root@froggitti.xyz:/home/cozmo/Desktop/all_servers/Viccyware-ota-server-dev/vicw-dev/otas/full/dev/0.5.0."${INCREMENT}".ota
-scp -i "$SERVER_KEY_PATH" "_build/Viccyware-0.5.0.${INCREMENT}oskr.ota" root@froggitti.xyz:/home/cozmo/Desktop/all_servers/Viccyware-ota-server-dev/vicw-dev/otas/full/oskr/0.5.0."${INCREMENT}".ota
-scp -i "$SERVER_KEY_PATH" "_build/Viccyware-0.5.0.${INCREMENT}.ota" root@froggitti.xyz:/home/cozmo/Desktop/all_servers/Viccyware-ota-server-dev/vicw-dev/otas/full/prod/0.5.0."${INCREMENT}".ota
+
+# commented out cause prod builds are building with the "oskr" extension, so we need to SCP the oskr build to the server as soon as its done building
+#scp -i "$SERVER_KEY_PATH" "_build/Viccyware-0.5.0.${INCREMENT}oskr.ota" root@froggitti.xyz:/home/cozmo/Desktop/all_servers/Viccyware-ota-server-dev/vicw-dev/otas/full/oskr/0.5.0."${INCREMENT}".ota
+
+# this is a prod build
+scp -i "$SERVER_KEY_PATH" "_build/Viccyware-0.5.0.${INCREMENT}oskr.ota" root@froggitti.xyz:/home/cozmo/Desktop/all_servers/Viccyware-ota-server-dev/vicw-dev/otas/full/prod/0.5.0."${INCREMENT}".ota
 
 # leave these commented unless we want to start copying otas to a never-changing URL
 #scp -i ~/id_rsa_ROOT_COZMOSERVER "_build/Viccyware-0.5.0.${INCREMENT}d.ota" root@froggitti.xyz:/home/cozmo/Desktop/all_servers/Viccyware-ota-server-dev/vicw-dev/otas/full/latest/dev.ota
